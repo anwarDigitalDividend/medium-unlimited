14edbfba2023e57aa420ae659459a59f9c12abe9
anwarDigitalDividend <60212592+anwarDigitalDividend@users.noreply.github.com>
2020-06-26 20:42:01 +0500

    Add extra domain

--- src/request_interceptors.js
+++ src/request_interceptors.js
@@ -34,6 +34,7 @@ const urlsList = [
   'https://robinhood.engineering/*',
   'https://blog.hipolabs.com/*',
   'https://ux.shopify.com/*',
+  'https://blog.echobind.com/*'
 ];
 
 export default function intercept() {
